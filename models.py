6bfc5c55d5584741abbf924c9aa7c28d02a89c2d
Eric Chen <eric.chen@dauphine.eu>
2025-11-25 09:22:45 +0100

    requirement, utils et models cnn

--- models.py
+++ models.py
@@ -16,11 +16,25 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         ### To do 4
-        raise NotImplementedError
-
+        self.conv1=nn.Conv2d(3, 32, 5, 1,2)
+        self.conv2=nn.Conv2d(32,64,5,1,2)
+        self.conv3=nn.Conv2d(64,128,5,1,2)
+        self.activation=nn.ReLU()
+        self.pool=nn.MaxPool2d(2,2)
+        self.fc1=nn.Linear(2048,512)
+        self.fc2=nn.Linear(512,10)
     def forward(self, x):
         ### To do 4
-        raise NotImplementedError
+        x=self.activation(self.conv1(x))
+        x=self.pool(x)
+        x=self.activation(self.conv2(x))
+        x=self.pool(x)
+        x=self.activation(self.conv3(x))
+        x=self.pool(x)
+        x=x.view(x.size(0),-1)
+        x=self.activation(self.fc1(x))
+        x=self.fc2(x)
+        return x
 
 class ResNet18(nn.Module):
     def __init__(self):
